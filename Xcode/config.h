159e0a80f911c0b05cc46af09417fe4d43abc013
Makhaev Anatoliy <Makhaev_A@dep7mail.com>
2020-02-19 11:24:22 +0300

    Initial commit

---
+++ Xcode/config.h
@@ -0,0 +1,25 @@
+/* config.h.  Manually generated for Xcode.  */
+
+/* Default visibility */
+#define DEFAULT_VISIBILITY /**/
+
+/* Message logging */
+#define ENABLE_LOGGING 1
+
+/* Define to 1 if the system has the type `nfds_t'. */
+#define HAVE_NFDS_T 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Darwin backend */
+#define OS_DARWIN 1
+
+/* Use POSIX poll() implementation */
+#define POLL_POSIX 1
+
+/* Use POSIX Threads */
+#define THREADS_POSIX 1
+
+/* Use GNU extensions */
+#define _GNU_SOURCE 1
